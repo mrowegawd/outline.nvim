c1712cab12b89f9f5488d216199d90c3481791e9
mrowegawd <7404751+mrowegawd@users.noreply.github.com>
2024-08-05 20:10:33 +0700

    fix(highlights+sidebar): just want to get rid `invalid buf/win` warnings

--- lua/outline/sidebar.lua
+++ lua/outline/sidebar.lua
@@ -386,18 +386,29 @@ function Sidebar:__goto_location(change_focus)
     return
   end
 
-  vim.api.nvim_win_set_cursor(self.code.win, { node.line + 1, node.character })
+  if type(self.code.buf) == 'number' and vim.api.nvim_buf_is_valid(self.code.buf) then
+    local line_count = vim.api.nvim_buf_line_count(self.code.buf)
+    if node.line > 0 and line_count > node.line then
+      vim.api.nvim_win_set_cursor(self.code.win, { node.line + 1, node.character })
+    end
+  end
 
   if cfg.o.outline_window.center_on_jump then
     vim.fn.win_execute(self.code.win, 'normal! zz')
   end
 
-  utils.flash_highlight(
-    self.code.win,
-    node.line + 1,
-    cfg.o.outline_window.jump_highlight_duration,
-    'OutlineJumpHighlight'
-  )
+  if
+    type(self.code.buf) == 'number'
+    and vim.api.nvim_buf_is_valid(self.code.buf)
+    and vim.api.nvim_win_is_valid(self.code.win)
+  then
+    utils.flash_highlight(
+      self.code.win,
+      node.line + 1,
+      cfg.o.outline_window.jump_highlight_duration,
+      'OutlineJumpHighlight'
+    )
+  end
 
   if change_focus then
     vim.fn.win_gotoid(self.code.win)
